7df98860ea5e22a242d32834a66919d6351129ac
Binyamin Roberts <jzroberts@hotmail.com>
2022-08-22 12:43:48 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '02da87b6-9cf0-41a5-b544-9e5e21df98fb';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/searchView.js
@@ -0,0 +1,26 @@
+class SearchView {
+  _parentEl = document.querySelector('.search');
+
+  getQuery() {
+    const query = this._parentEl.querySelector('.search__field').value;
+    this._clearInput();
+    return query;
+  }
+
+  _clearInput() {
+    this._parentEl.querySelector('.search__field').value = '';
+  }
+
+  addHandlerSearch(handler) {
+    this._parentEl.addEventListener('submit', function (e) {
+      e.preventDefault();
+      handler();
+    });
+  }
+}
+
+export default new SearchView();
+
+//
+
+// The issue is that you are attempting to access a property of a null value (null.href). The point of the change that was in that answer was to check for a null or undefined value prior to attempting to access the property.
